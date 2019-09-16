9887fafe57a459abfb4124c29d2b83ca12262edc
lisaw97 <lisawen0614@gmail.com>
2019-09-15 19:34:00 -0700

    update class inheritance

--- W4D2/class_inheritance/employee.rb
+++ W4D2/class_inheritance/employee.rb
@@ -1,18 +1,21 @@
 class Employee
+  attr_reader :name, :title, :salary
 
-  attr_reader :name, :title, :salary, :boss  
-
-  def initialize(name, title, salary, boss)
+  def initialize(name, title, salary, boss = nil)
     @name = name
     @title = title
     @salary = salary
+    self.boss = boss
+  end
+
+  def boss=(boss)
     @boss = boss
+    boss.employees << self if !boss.nil?
   end
 
   def bonus(multiplier)
-    bonus = @salary * multiplier
+    @salary * multiplier
   end
+
 end
-
-
 
--- W4D2/class_inheritance/manager.rb
+++ W4D2/class_inheritance/manager.rb
@@ -1,40 +1,40 @@
-require_relative 'employee.rb'
+require_relative 'employee'
 
 class Manager < Employee
 
   attr_reader :employees
 
-  def initialize(employees)
-    @employees = employees
+  def initialize(name, title, salary, boss = nil)
+    @employees = []
+    super
   end
 
   def bonus(multiplier)
-    salary_sum * multiplier
+    employee_salaries * multiplier
   end
-    
-  def salary_sum
-    # return salary if @employees.nil?
 
-    total = 0
+  def employee_salaries
+    sum = 0
     @employees.each do |employee|
       if employee.is_a?(Manager)
-        total += employee.salary_sum + employee.salary
-      else 
-        total += employee.salary
+        sum += employee.salary + employee.employee_salaries
+      else
+        sum += employee.salary
       end
     end
-    total
+    sum
   end
-  
+
 end
 
-ned = Employee.new("Ned", "Founder", 1000000, nil)
-darren = Employee.new("Darren", "TA Manager", 78000, "Ned")
-shawna = Employee.new("Shawna", "TA", 12000, "Darren")
-david = Employee.new("David", "TA", 10000, "Darren")
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+ #employees = [darren]
+ #bonus = 78000
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+ #employees = [shawna, david]
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
 
-ned = Manager.new([darren])
-darren = Manager.new([shawna, david])
 
 puts ned.bonus(5) # => 500_000
 puts darren.bonus(4) # => 88_000
